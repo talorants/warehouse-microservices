a7bc64516e43e8370a56c44f3cd10b253b611c17
Nuriddin321 <donkoko1996@gmail.com>
2022-08-28 19:00:50 +0500

    created new project and  Shared.Entity, Shared.Model

---
+++ src/Talorants.Shared.Models/BaseResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace Talorants.Shared.Model;
+
+public class BaseResponse
+{
+    public bool IsSuccess { get; set; }
+    public string? ErrorMessage { get; set; }
+    
+    public BaseResponse(bool isSuccess) => IsSuccess = isSuccess;
+    public BaseResponse(string errorMessage) => ErrorMessage = errorMessage;
+
+    public BaseResponse(bool isSuccess, string errorMessage)
+    {   
+        IsSuccess = isSuccess;
+        ErrorMessage = errorMessage;
+    }
+}
+
+public class BaseResponse<T> : BaseResponse
+{
+    public T? Data { get; set; }
+    public BaseResponse(bool isSuccess) : base(isSuccess) { }
+    public BaseResponse(string? errorMessage) : base(errorMessage) { }
+    public BaseResponse(bool isSuccess, string? errorMessage) : base(isSuccess, errorMessage) { }
+}
